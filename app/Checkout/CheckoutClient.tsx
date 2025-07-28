a3c37404f3bfb1e85e5e17e02cd4cb013c7f68dd
shereen <shereenshawky2001@gmail.com>
2025-07-28 17:41:15 +0200

    fixing issues in checkout

--- app/Checkout/CheckoutClient.tsx
+++ app/Checkout/CheckoutClient.tsx
@@ -2,9 +2,9 @@
 
 import React, { useEffect, useState } from 'react';
 import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
 import CheckoutForm from './_components/CheckoutForm';
 import { useSearchParams } from 'next/navigation';
+import { loadStripe, StripeElementsOptions } from '@stripe/stripe-js';
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
@@ -41,16 +41,18 @@ export default function CheckoutClient() {
     return <div className="text-center mt-20">Preparing payment...</div>;
   }
 
-  const options = {
-    clientSecret,
-    appearance: {
-      theme: 'stripe',
-    },
-  };
 
-  return (
-    <Elements stripe={stripePromise} options={options}>
-      <CheckoutForm amount={amount} />
-    </Elements>
-  );
+const options: StripeElementsOptions = {
+  clientSecret: clientSecret!,
+  appearance: {
+    theme: 'flat',
+  },
+};
+
+return (
+  <Elements stripe={stripePromise} options={options}>
+    <CheckoutForm amount={amount} />
+  </Elements>
+);
+
 }
